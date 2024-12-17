9bd090eb26206f45e9a28d4967003a41e4599d9a
cedkeChat <123301168+00cedke@users.noreply.github.com>
2024-12-17 20:53:50 +0100

    Create sum.go

---
+++ sum.go
@@ -0,0 +1,13 @@
+// == Credits Pretendo Network == //
+
+package nex
+
+import "golang.org/x/exp/constraints"
+
+func sum[T, O constraints.Integer](data []T) O {
+	var result O
+	for _, b := range data {
+		result += O(b)
+	}
+	return result
+}
